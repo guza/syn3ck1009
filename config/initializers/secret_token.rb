3bf19ae9137b555eaf3eca6e2d2b92d6fe9c8cbc
superrookie <superrookie.com@gmail.com>
2014-10-09 07:10:57 +0000

    syn3ck 1009 first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syn3ck::Application.config.secret_key_base = '01d52578dbfee6ba6158c27dd82a7316dbb1bce54d2367355816297e556d6793297b69f9a025ddf5798429ebb8913e47650c5ee0e83db2f68f12f12b8606df0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Syn3ck::Application.config.session_store :cookie_store, key: '_syn3ck_session'
